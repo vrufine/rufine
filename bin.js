092e2b9488298c295ff455adae9757dff8210dba
Vinicius Rufine <vsrufine@gmail.com>
2018-08-18 00:00:10 -0300

    bin infos

---
+++ bin.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log(JSON.stringify({
+  name: 'Vinicius Rufine',
+  github: 'https://github.com/vrufine',
+  email: 'vsrufine@gmail.com',
+  linkedin: 'https://www.linkedin.com/in/vinicius-rufine-69a11a43/',
+  facebook: 'https://www.facebook.com/viniciusrufine',
+  telegram: 'https://telegram.me/vrufine'
+}, null, 2))
